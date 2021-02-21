ae39a23d44f45b8259752f9e98a650d0f072e177
Rhys Wakeham <Rhys.Wakeham@bighand.com>
2021-02-21 21:54:02 +0000

    Styling the application

--- Pomorillo/Pomorillo.WPFApplication/MainWindow.xaml.cs
+++ Pomorillo/Pomorillo.WPFApplication/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Pomorillo.WPFApplication
 {
@@ -23,6 +11,21 @@ namespace Pomorillo.WPFApplication
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MinimiseButtonClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.DragMove();
         }
     }
 }
